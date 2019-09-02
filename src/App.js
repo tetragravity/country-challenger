1127b1f91b16d3192172cbe12fad40e18ee9017d
Marcus <marcusgnf@gmail.com>
2019-09-02 14:49:55 -0500

    Fetch Countries Added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch } from "react-router-dom";
 import Home from './components/Home';
@@ -9,24 +9,34 @@ import CountriesYouShouldVisit from './components/CountriesYouShouldVisit';
 import ExpensiveCountries from './components/ExpensiveCountries';
 import ErrorPage from './components/ErrorPage';
 
-function App() {
-  return (
-    <div className="App">
-    <div className="Router">
-        <div>
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/all-countries/" component={AllCountries} />
-                <Route path="/cheap-countries/" component={CheapCountries} />
-                <Route path="/countries-visited/" component={CountriesVisited} />
-                <Route path="/countries-you-should-visit/" component={CountriesYouShouldVisit} />
-                <Route path="/expensive-countries/" component={ExpensiveCountries} />
-                <Route path="/404-not-found/" component={ErrorPage} />
-            </Switch>
-      </div>
-    </div>
-    </div>
-  );
-}
+// Const Variable for all REST COUNTRIES API (LARGE DATA)
+const ALL_COUNTRIES = "https://restcountries.eu/rest/v2/all"
 
+class App extends Component {
+ 
+  componentDidMount() {
+    fetch(ALL_COUNTRIES)
+      .then(response => response.json())
+      .then(Countrydata => {console.log(Countrydata)});
+  }
+
+
+  render(){
+    return(
+      <div className="App">
+      <div>
+          <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/all-countries/" component={AllCountries} />
+              <Route path="/cheap-countries/" component={CheapCountries} />
+              <Route path="/countries-visited/" component={CountriesVisited} />
+              <Route path="/countries-you-should-visit/" component={CountriesYouShouldVisit} />
+              <Route path="/expensive-countries/" component={ExpensiveCountries} />
+              <Route path="/404-not-found/" component={ErrorPage} />
+          </Switch>
+    </div>
+  </div>
+    )
+  }
+}
 export default App;
